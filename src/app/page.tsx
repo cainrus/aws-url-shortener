6121ff75412eeda6dca97b850d8fd4059711018e
Sergei Tarassov <starassov@leongaming.com>
2025-01-06 00:00:47 +0200

    chore: save point

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
   const [isPending, startTransition] = useTransition();
   const [isURLValid, setIsURLValid] = useState(true);
   const [url, setURL] = useState<{ short: string; long: string } | null>(null);
+  const [copySuccess, setCopySuccess] = useState(false);
 
   async function handleSubmit(formData: FormData) {
     const longUrl = formData.get('url') as string;
@@ -34,6 +35,18 @@ export default function Home() {
     }
   }
 
+  async function handleCopy() {
+    if (url) {
+      try {
+        await navigator.clipboard.writeText(`${location.host}/${url.short}`);
+        setCopySuccess(true);
+        setTimeout(() => setCopySuccess(false), 2000); // Reset success message after 2 seconds
+      } catch (error) {
+        console.error('Failed to copy:', error);
+      }
+    }
+  }
+
   return (
       <main className="grid place-items-center h-screen">
         <div className="bg-cyan-900 w-full grid place-content-center py-20 px-6 sm:px-16 lg:px-32">
@@ -48,16 +61,16 @@ export default function Home() {
                 placeholder="Enter link here"
                 className={`block w-full rounded-md border py-3 px-3 pr-14 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset outline-none ${
                     isURLValid
-                        ? "focus:ring-cyan-600 border-gray-300"
-                        : "focus:ring-red-600 border-red-600 ring-red-600"
+                        ? 'focus:ring-cyan-600 border-gray-300'
+                        : 'focus:ring-red-600 border-red-600 ring-red-600'
                 }`}
             />
             <button
                 type="submit"
                 className={`absolute inset-y-0 right-0 py-1.5 px-3 text-white font-semibold rounded-md ${
                     isPending
-                        ? "bg-cyan-400 cursor-not-allowed"
-                        : "bg-cyan-600 hover:bg-cyan-700"
+                        ? 'bg-cyan-400 cursor-not-allowed'
+                        : 'bg-cyan-600 hover:bg-cyan-700'
                 }`}
                 disabled={isPending}
             >
@@ -69,20 +82,25 @@ export default function Home() {
           )}
           {url && (
               <div className="flex flex-col gap-2 mt-6 p-5 rounded-md border border-cyan-500 bg-cyan-50">
-                <p className="text-cyan-700 font-semibold">
-                  {" "}
-                  <a
-                      href={`/${url.short}`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="underline hover:text-cyan-800"
+                <div className="flex items-center justify-between">
+                  <p className="text-cyan-700 font-semibold">
+                    <a
+                        href={`/${url.short}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="underline hover:text-cyan-800"
+                    >
+                      {location.host}/{url.short}
+                    </a>
+                  </p>
+                  <button
+                      onClick={handleCopy}
+                      className="text-cyan-700 bg-cyan-100 hover:bg-cyan-200 p-2 rounded-md font-medium text-sm"
                   >
-                    {location.host}/{url.short}
-                  </a>
-                </p>
-                <p className="text-gray-700 line-clamp-1">
-                  {url.long}
-                </p>
+                    {copySuccess ? 'Copied!' : 'Copy'}
+                  </button>
+                </div>
+                <p className="text-gray-700 line-clamp-1">{url.long}</p>
               </div>
           )}
         </div>
